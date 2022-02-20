f5c9d9d5e7636d192c491eac2be3056e68f2b9cc
ruanvalente <ruanopensuselinux@gmail.com>
2022-02-19 22:18:38 -0300

    chore: add plot for generate component

---
+++ src/components/Example/test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+
+import Example from '.'
+
+describe('<Example />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<Example />)
+
+    expect(
+      screen.getByRole('heading', { name: /Example/i })
+    ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
